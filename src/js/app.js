124d9be2bd9eaa3d1326e0d100f6e3149ef34239
whycry <stt.orlova@gmail.com>
2023-05-16 16:23:24 +0300

    hw

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
 export default class Validator {
   static validateUsername(username) {
     const ifMistake = [
-      /[^A-Za-z0-9_-]/.test(username),
-      /\d{3}/.test(username),
-      /^(\d|_|-)/.test(username),
-      /(\d|_|-)$/.test(username),
+      /[^A-Za-z0-9_-]/.test(username), // проверка на на наличие других символов кроме латинских букв, -, _ и цифр (0-9)
+      /\d{3}/.test(username), // проверка на наличие 3х цифр подряд
+      /^(\d|_|-)/.test(username), // проверка на цифру, -, _ в начале
+      /(\d|_|-)$/.test(username), // проверка на цифру, -, _ в конце
     ];
 
     if (ifMistake.includes(true)) {
